fff12e06071dabd199b0b56f73516fc0020736d8
Glen Wasserfall <glen@wasserfalls.co.za>
2019-12-06 00:02:52 +0200

    testing

--- build.py
+++ build.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 debug = False if os.getenv("DEBUG").lower() == "false" else True
 
-@app.route('/')
-def hello_world():
+@app.route('/webhooks/611f19e75e9a482ecbafee68509d16e2a708711c3a311d95c4106ee7c4ab4c6c', methods=["POST"])
+def webhook():
     return 'Hello, World!'
 
 
