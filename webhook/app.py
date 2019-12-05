ccb133041725232fe52b4fbc28a229a5c5c1cb70
Glen Wasserfall <glen@wasserfalls.co.za>
2019-12-06 01:47:31 +0200

    update

--- webhook/app.py
+++ webhook/app.py
@@ -1,18 +1,46 @@
+from subprocess import Popen, PIPE, STDOUT
 from flask import Flask, request
 from dotenv import load_dotenv
 
 import hashlib
+import smtplib
 import hmac
 import json
 import os
 
+
 load_dotenv()
-
 app = Flask(__name__)
-
 debug = False if os.getenv("DEBUG").lower() == "false" else True
 
 
+def send_email(output):
+	gmail_user = os.getenv("GMAIL_USER")
+	gmail_password = os.getenv("GMAIL_PASS")
+
+	sent_from = gmail_user
+	to = [gmail_user]
+	subject = 'Matcha:: Build Report'
+	body = output
+
+	email_text = """\
+	From: %s
+	To: %s
+	Subject: %s
+
+	%s
+	""" % (sent_from, ", ".join(to), subject, body)
+
+	try:
+		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+		server.ehlo()
+		server.login(gmail_user, gmail_password)
+		server.sendmail(sent_from, to, email_text)
+		server.close()
+
+		print 'Email sent!'
+	except:
+		print 'Something went wrong...'
 
 @app.route('/611f19e75e9a482ecbafee68509d16e2a708711c3a311d95c4106ee7c4ab4c6c', methods=["POST"])
 def webhook():
@@ -23,6 +51,11 @@ def webhook():
 		github_sig = request.headers.get('X-Hub-Signature')
 
 		if github_sig == signature:
+
+			p = Popen("build.sh", shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+			output = p.stdout.read()
+			send_mail(output)
+
 			return "OK", 200
 		else:
 			return "Who are you?", 500
---
+++ webhook/build.sh
@@ -0,0 +1,1 @@
+echo "Some output here"
