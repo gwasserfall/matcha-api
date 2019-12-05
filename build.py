b7e5e260464eb2032865aa675def8dbe648f72b5
Glen Wasserfall <glen@wasserfalls.co.za>
2019-12-05 23:28:13 +0200

    testing

--- build.py
+++ build.py
@@ -5,11 +5,14 @@ import os
 
 load_dotenv()
 
-
 app = Flask(__name__)
 
-
 debug = False if os.getenv("DEBUG").lower() == "false" else True
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(debug=debug, port=9999)
