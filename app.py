122925f4830b6c138ea5ad3878d3d783ee8ec3dd
Glen Wasserfall <glen@wasserfalls.co.za>
2019-12-13 05:48:26 +0200

    sockets, moving to branch

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 from flask_restful import Resource, Api
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 from flask_jwt_extended import JWTManager
+
 
 from helpers import jwt_refresh_required
 from helpers import ModelEncoder
@@ -28,10 +29,34 @@ api.add_resource(UserResource, "/user/<int:id>")
 api.add_resource(LoginResource, "/login")
 
 
+@app.route("/")
+def socket():
+    return render_template("sockets.html")
+
+
+clients = []
+
+@socketio.on('connect')
+def test_connect():
+    print(request.sid + " has joined the fold")
+    clients.append(request.sid)
+    room = session.get('room')
+    print(room)
+    join_room(room)
+    print(clients)
+    socketio.emit('reply', {'message': 'has entered the room.'}, room=clients[-1])
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print("Disconnected")
+
 # Need to move to seperate file
 @socketio.on('message')
 def handle_message(message):
-    print('received message: ' + message)
+    print(request.sid)
+    socketio.emit("reply", {"message" : "We got " + message["message"]})
+    print('received message: ' + message["message"])
+    print(clients)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
